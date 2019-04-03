3b4770f7c10ba05a2eefce39864b43d15ea65cfb
Benjamin Morel <benjamin.morel@gmail.com>
2019-04-03 13:10:09 +0200

    Add string types tests

---
+++ tests/StringTypesTest.php
@@ -0,0 +1,180 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BenMorel\OpenApiSchemaToJsonSchema\Tests;
+
+use BenMorel\OpenApiSchemaToJsonSchema\Convert;
+use PHPUnit\Framework\TestCase;
+
+class StringTypesTest extends TestCase
+{
+    public function testPlainStringIsUntouched() : void
+    {
+        $schema = json_decode(<<<'JSON'
+            {
+                "type": "string"
+            }
+JSON
+        );
+
+        $expected = json_decode(<<<'JSON'
+            {
+                "$schema": "http://json-schema.org/draft-04/schema#",
+                "type": "string"
+            }
+JSON
+        );
+
+        $result = Convert::openapiSchemaToJsonSchema($schema);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testDateRetained() : void
+    {
+        $schema = json_decode(<<<'JSON'
+            {
+                "type": "string",
+                "format": "date"
+            }
+JSON
+        );
+
+        $expected = json_decode(<<<'JSON'
+            {
+                "$schema": "http://json-schema.org/draft-04/schema#",
+                "type": "string",
+                "format": "date"
+            }
+JSON
+        );
+
+        $result = Convert::openapiSchemaToJsonSchema($schema);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testDateConvertedToDateTime() : void
+    {
+        $schema = json_decode(<<<'JSON'
+            {
+                "type": "string",
+                "format": "date"
+            }
+JSON
+        );
+
+        $expected = json_decode(<<<'JSON'
+            {
+                "$schema": "http://json-schema.org/draft-04/schema#",
+                "type": "string",
+                "format": "date-time"
+            }
+JSON
+        );
+
+        $result = Convert::openapiSchemaToJsonSchema($schema, [
+            'dateToDateTime' => true
+        ]);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testHandlesByteFormat() : void
+    {
+        $schema = json_decode(<<<'JSON'
+            {
+                "type": "string",
+                "format": "byte"
+            }
+JSON
+        );
+
+        $expected = json_decode(<<<'JSON'
+            {
+                "$schema": "http://json-schema.org/draft-04/schema#",
+                "type": "string",
+                "format": "byte",
+                "pattern": "^[\\w\\d+\\/=]*$"
+            }
+JSON
+        );
+
+        $result = Convert::openapiSchemaToJsonSchema($schema);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testRetainsCustomFormats() : void
+    {
+        $schema = json_decode(<<<'JSON'
+            {
+                "type": "string",
+                "format": "custom_email"
+            }
+JSON
+        );
+
+        $expected = json_decode(<<<'JSON'
+            {
+                "$schema": "http://json-schema.org/draft-04/schema#",
+                "type": "string",
+                "format": "custom_email"
+            }
+JSON
+        );
+
+        $result = Convert::openapiSchemaToJsonSchema($schema);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testRetainsPasswordFormat() : void
+    {
+        $schema = json_decode(<<<'JSON'
+            {
+                "type": "string",
+                "format": "password"
+            }
+JSON
+        );
+
+        $expected = json_decode(<<<'JSON'
+            {
+                "$schema": "http://json-schema.org/draft-04/schema#",
+                "type": "string",
+                "format": "password"
+            }
+JSON
+        );
+
+        $result = Convert::openapiSchemaToJsonSchema($schema);
+
+        $this->assertEquals($expected, $result);
+    }
+
+    public function testRetainsBinaryFormat() : void
+    {
+        $schema = json_decode(<<<'JSON'
+            {
+                "type": "string",
+                "format": "binary"
+            }
+JSON
+        );
+
+        $expected = json_decode(<<<'JSON'
+            {
+                "$schema": "http://json-schema.org/draft-04/schema#",
+                "type": "string",
+                "format": "binary"
+            }
+JSON
+        );
+
+        $result = Convert::openapiSchemaToJsonSchema($schema);
+
+        $this->assertEquals($expected, $result);
+    }
+}
